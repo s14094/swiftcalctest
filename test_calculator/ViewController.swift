e4d9b4bccf49c5a4478e3da340506a8198fcf9ce
ActumLab <actumlab@rafas-macbook-air.local>
2017-07-20 15:35:44 +0200

    alpha v1

--- test_calculator/ViewController.swift
+++ test_calculator/ViewController.swift
@@ -6,61 +6,103 @@
 //  Copyright © 2017 ActumLab. All rights reserved.
 //
 
+
+// TODO
+// if last was = then after clicking number reset
+// add new window with last number picked (and) operation
+// fix bug that you cant dobule click button
+
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var numberOnScreen:Double = 0;
     var previousNumber:Double = 0;
+    var performingMath = false
+    var operation = 0;
     
-    
-
     @IBOutlet weak var label: UILabel!
     
-    @IBAction func buttons(_ sender: UIButton) {
-        if label.text != "" && sender.tag != 15 && sender.tag != 16
+    @IBAction func numbers(_ sender: UIButton)
+    {
+        if performingMath == true
         {
-            if sender.tag == 11 // /
-            {
-                label.text = "/"
-            }
-            else if sender.tag == 12 // x
-            {
-                label.text = "x"
-            }
-            else if sender.tag == 13 // -
-            {
-                label.text = "-"
-            }
-            else if sender.tag == 14 // +
-            {
-                label.text = "+"
-            }
-        }
-    }
-    
-    @IBAction func numbers(_ sender: UIButton) {
-        if sender.tag == 10
-        {
-            label.text = label.text! + "0"
+            label.text = String(sender.tag-1)
+            numberOnScreen = Double(label.text!)!
+            performingMath = false
         }
         else
         {
-            label.text = label.text! + String(sender.tag)
+            label.text = label.text! + String(sender.tag-1)
+            numberOnScreen = Double(label.text!)!
         }
-        numberOnScreen = Double(label.text!)!
     }
+    
+    @IBAction func buttons(_ sender: UIButton)
+    {
+        if label.text != "" && sender.tag != 11 && sender.tag != 16
+        {
+            previousNumber = Double(label.text!)!
+            
+            if sender.tag == 12 //Divide
+            {
+                label.text = "/";
+            }
+            else if sender.tag == 13 //Multiply
+            {
+                label.text = "x";
+            }
+            else if sender.tag == 14 //Minus
+            {
+                label.text = "-";
+            }
+            else if sender.tag == 15 //Plus
+            {
+                label.text = "+";
+            }
+            
+            operation = sender.tag
+            performingMath = true;
+        }
+        else if sender.tag == 16
+        {
+            if operation == 12
+            {
+                label.text = String(previousNumber / numberOnScreen)
+            }
+            else if operation == 13
+            {
+                label.text = String(previousNumber * numberOnScreen)
+            }
+            else if operation == 14
+            {
+                label.text = String(previousNumber - numberOnScreen)
+            }
+            else if operation == 15
+            {
+                label.text = String(previousNumber + numberOnScreen)
+            }
+        }
+        else if sender.tag == 11
+        {
+            label.text = ""
+            previousNumber = 0;
+            numberOnScreen = 0;
+            operation = 0;
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
